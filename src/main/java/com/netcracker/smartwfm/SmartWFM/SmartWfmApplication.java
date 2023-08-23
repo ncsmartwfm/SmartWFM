c350d191a595a9485c5b468277ed89f0f46cc77a
Santhosh Kaitheri <sank1220@netcracker.com>
2023-08-23 10:38:14 +0530

    Initial commit

---
+++ src/main/java/com/netcracker/smartwfm/SmartWFM/SmartWfmApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.smartwfm.SmartWFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWfmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netcracker/smartwfm/SmartWFM/SmartWfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.smartwfm.SmartWFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
