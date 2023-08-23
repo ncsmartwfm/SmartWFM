f7e2169d82aaf0212121dcb9e7ee122bcbb0169b
Manvendra Verma <manvendrav@gmail.com>
2023-08-23 17:20:14 +0530

    Added DB related code and removed unwanted code

--- ModuleLineManager/src/main/java/com/netcracker/smartwfm/service/OpenDemandDaoService.java
+++ ModuleLineManager/src/main/java/com/netcracker/smartwfm/service/OpenDemandDaoService.java
@@ -2,47 +2,32 @@ package com.netcracker.smartwfm.service;
 
 import com.netcracker.smartwfm.dao.Demand;
 import com.netcracker.smartwfm.exception.DemandNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OpenDemandDaoService {
-    public static List<Demand> listOfDemands = new ArrayList<>();
-
-    static {
-        Demand demand1 = new Demand("332xxd3", "DT", Arrays.asList("Java", "OPF"),
-                4, "OPF", "Germany", "India");
-        Demand demand2 = new Demand("43f2sc", "DU", Arrays.asList("CPQ", "OPF"),
-                2, "OPF", "UAE", "India");
-        Demand demand3 = new Demand("4334fc2wd2", "NUDAY",
-                Arrays.asList("Java", "OPF", "SpringBoot"), 8, "OPF", "France",
-                "CIS");
-        listOfDemands.add(demand1);
-        listOfDemands.add(demand2);
-        listOfDemands.add(demand3);
-    }
-
-    // TODO find all the open demands
+    @Autowired
+    private DemandRepository demandRepository;
     public List<Demand> getAllDemands() {
-        return listOfDemands;
+        return demandRepository.findAll();
     }
 
     public void save(Demand demand) {
-        listOfDemands.add(demand);
+        demandRepository.save(demand);
     }
 
     public void deleteDemandById(String Id) {
-        Demand demand = listOfDemands.stream().filter(e -> e.getId().equals(Id)).findFirst().orElse(null);
-        if (demand == null) {
-            throw new DemandNotFoundException("Demand with demand Id:" + Id + " does not exist");
+        Optional<Demand> demandOptional = demandRepository.findById(Id);
+        if(demandOptional.isPresent()) {
+            demandRepository.delete(demandOptional.get());
         }
-        listOfDemands.remove(demand);
+        throw new DemandNotFoundException("Demand for the Id "+Id+" does not exist");
+
     }
-
-    // TODO to save the open demand in the DB
-
-    // TODO to delete the open demand from the DB
 }
