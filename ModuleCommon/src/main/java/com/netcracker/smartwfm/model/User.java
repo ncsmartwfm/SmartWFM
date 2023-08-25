8ccb4324f65da64b2a914ad5d7ae950f6d519089
ncsmartwfm <ncsmartwfm@gmail.com>
2023-08-25 09:28:03 +0530

    POM , User class update

---
+++ ModuleCommon/src/main/java/com/netcracker/smartwfm/model/User.java
@@ -0,0 +1,57 @@
+package com.netcracker.smartwfm.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userName;
+
+    public User(String userName, String password, String fullName) {
+        this.userName = userName;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    private String password;
+    private String fullName;
+}
